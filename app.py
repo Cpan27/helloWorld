ec4aec0137d3c8747ab45d22333218edf92ffe24
Cassie Ying Pan <Cpan27@terpmail.umd.edu>
2026-09-24 12:14:48 -0400

    Added a new route for about HTML page.

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here.
     return 'Hello World from Cassie Pan! I am adding my first code change.'
+
 @app.route('/hello')
 def hello():  # put application's code here.
     return render_template('hello.html')
+
+@app.route('/about')
+def about():  # put application's code here.
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
